560d79ddaa76a0040a6f5be40719323fba3852df
Parth-Goyal11 <68484923+Parth-Goyal11@users.noreply.github.com>
2021-02-15 12:26:50 -0500

    Update Uddered_But_Not_Herd.java

--- Uddered_But_Not_Herd.java
+++ Uddered_But_Not_Herd.java
@@ -13,26 +13,26 @@ public class Uddered_But_Not_Herd {
         Scanner input = new Scanner(System.in);
         ArrayList<String> letters = new ArrayList<String>();
         ArrayList<Integer> indexes = new ArrayList<Integer>();
-        String cowphabet = input.nextLine();
-        String lower_string = input.nextLine();
-        int iteration = 1;
+        String cowphabet = input.nextLine(); //Take in the input for the cow alphabet
+        String lower_string = input.nextLine();//Take in input for the string Farmer John heard.
+        int iteration = 1;                     //Variable iteration to keep track of the number of times through the alphabet.
         int lowerLength = lower_string.length();
-        for(int i =0; i<lowerLength; i++ ){
+        for(int i =0; i<lowerLength; i++ ){    //Add the seperate of letters of the string into a ArrayList so it is easier to work with
             char charForm = lower_string.charAt(i);
             String stringForm = Character.toString(charForm);
-            letters.add(stringForm);
+            letters.add(stringForm);                  
         }
 
-        for (int z=0; z<letters.size(); z++){
+        for (int z=0; z<letters.size(); z++){   //Get the indexes of each letter of the string in the cowphabet.
             String check = letters.get(z);
             int currentInstance = cowphabet.indexOf(check);
             indexes.add(currentInstance);
         }
-        for (int a = 0; a<indexes.size(); a++){
-            if(a == 0){
+        for (int a = 0; a<indexes.size(); a++){ //If a is 0 do nothing because that is the first index.
+            if(a == 0){                         
 
             }else{
-                if(indexes.get(a) <= indexes.get(a-1)) {
+                if(indexes.get(a) <= indexes.get(a-1)) { // if a set index is less or equal to the previous index, increase the iteration by one.
                     iteration += 1;
 
                 }
